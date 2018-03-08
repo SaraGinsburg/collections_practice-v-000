ee1c4c800bdf6cd5761f37fe3c2f184c143df977
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-08 22:50:08 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,9 +14,15 @@ def sort_array_char_count(arr)
   result = arr.sort_by {|x| x.length}
 end
 
+def swap_elements(arr)
+  hold = arr[1]
+  arr[1] = arr[2]
+  arr[2] = hold
+end
+
+
 def swap_elements_from_to(arr, m, n)
   hold = arr[m]
   arr[m] = arr[n]
   arr[n] = hold
-
 end
